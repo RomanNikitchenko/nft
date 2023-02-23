63874e5406f0eae9c6903fc79dbacea93714c98c
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-02-23 12:35:43 +0200

    deploy

---
+++ js/mobile-menu.js
@@ -0,0 +1,5 @@
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+})();
