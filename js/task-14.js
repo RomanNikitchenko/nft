4ddced1bf3c86daef23752b7fdfc27e63f222eaf
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-02-23 21:34:20 +0200

    counter-clockwise

--- js/task-14.js
+++ js/task-14.js
@@ -3,7 +3,7 @@ const items = document.querySelectorAll('.list .box');
 const centralImage = document.querySelectorAll('.central-image');
 const btn = document.querySelector('.btn');
 
-let limit = 1;
+let limit = 0;
 let disabled = false;
 
 //navigation menu
@@ -16,7 +16,7 @@ btn.addEventListener('click', e => {
   setTimeout(() => (disabled = false), 600);
   if (e.target.tagName !== 'BUTTON') return;
 
-  if (limit === 1) {
+  if (limit === 0) {
     items.forEach(item => {
       if (item.classList.contains('motion-pause')) {
         item.classList.remove('motion-pause');
@@ -24,135 +24,135 @@ btn.addEventListener('click', e => {
     });
   }
 
-  if (limit > 1) toggleAnimation();
+  if (limit > 0) toggleAnimation();
 
-  if (limit < 7) {
+  if (limit < 6) {
     limit += 1;
   } else {
-    limit = 2;
+    limit = 1;
   }
 });
 
 function toggleAnimation() {
   items.forEach(item => {
-    if (limit === 2) {
+    if (limit === 1) {
       if (item.classList.contains('box-1')) {
-        item.classList.replace(`motion-move-1`, `motion-move-2`);
+        item.classList.replace(`motion-move-1`, `motion-move-6`);
       }
       if (item.classList.contains('box-2')) {
-        item.classList.replace(`motion-move-2`, `motion-move-3`);
+        item.classList.replace(`motion-move-2`, `motion-move-1`);
       }
       if (item.classList.contains('box-3')) {
-        item.classList.replace(`motion-move-3`, `motion-move-4`);
+        item.classList.replace(`motion-move-3`, `motion-move-2`);
       }
       if (item.classList.contains('box-4')) {
-        item.classList.replace(`motion-move-4`, `motion-move-5`);
+        item.classList.replace(`motion-move-4`, `motion-move-3`);
       }
       if (item.classList.contains('box-5')) {
-        item.classList.replace(`motion-move-5`, `motion-move-6`);
+        item.classList.replace(`motion-move-5`, `motion-move-4`);
       }
       if (item.classList.contains('box-6')) {
-        item.classList.replace(`motion-move-6`, `motion-move-1`);
+        item.classList.replace(`motion-move-6`, `motion-move-5`);
+      }
+    }
+    if (limit === 2) {
+      if (item.classList.contains('box-1')) {
+        item.classList.replace(`motion-move-6`, `motion-move-5`);
+      }
+      if (item.classList.contains('box-2')) {
+        item.classList.replace(`motion-move-1`, `motion-move-6`);
+      }
+      if (item.classList.contains('box-3')) {
+        item.classList.replace(`motion-move-2`, `motion-move-1`);
+      }
+      if (item.classList.contains('box-4')) {
+        item.classList.replace(`motion-move-3`, `motion-move-2`);
+      }
+      if (item.classList.contains('box-5')) {
+        item.classList.replace(`motion-move-4`, `motion-move-3`);
+      }
+      if (item.classList.contains('box-6')) {
+        item.classList.replace(`motion-move-5`, `motion-move-4`);
       }
     }
     if (limit === 3) {
       if (item.classList.contains('box-1')) {
-        item.classList.replace(`motion-move-2`, `motion-move-3`);
+        item.classList.replace(`motion-move-5`, `motion-move-4`);
       }
       if (item.classList.contains('box-2')) {
-        item.classList.replace(`motion-move-3`, `motion-move-4`);
+        item.classList.replace(`motion-move-6`, `motion-move-5`);
       }
       if (item.classList.contains('box-3')) {
-        item.classList.replace(`motion-move-4`, `motion-move-5`);
+        item.classList.replace(`motion-move-1`, `motion-move-6`);
       }
       if (item.classList.contains('box-4')) {
-        item.classList.replace(`motion-move-5`, `motion-move-6`);
+        item.classList.replace(`motion-move-2`, `motion-move-1`);
       }
       if (item.classList.contains('box-5')) {
-        item.classList.replace(`motion-move-6`, `motion-move-1`);
+        item.classList.replace(`motion-move-3`, `motion-move-2`);
       }
       if (item.classList.contains('box-6')) {
-        item.classList.replace(`motion-move-1`, `motion-move-2`);
+        item.classList.replace(`motion-move-4`, `motion-move-3`);
       }
     }
     if (limit === 4) {
       if (item.classList.contains('box-1')) {
-        item.classList.replace(`motion-move-3`, `motion-move-4`);
+        item.classList.replace(`motion-move-4`, `motion-move-3`);
       }
       if (item.classList.contains('box-2')) {
-        item.classList.replace(`motion-move-4`, `motion-move-5`);
+        item.classList.replace(`motion-move-5`, `motion-move-4`);
       }
       if (item.classList.contains('box-3')) {
-        item.classList.replace(`motion-move-5`, `motion-move-6`);
+        item.classList.replace(`motion-move-6`, `motion-move-5`);
       }
       if (item.classList.contains('box-4')) {
-        item.classList.replace(`motion-move-6`, `motion-move-1`);
+        item.classList.replace(`motion-move-1`, `motion-move-6`);
       }
       if (item.classList.contains('box-5')) {
-        item.classList.replace(`motion-move-1`, `motion-move-2`);
+        item.classList.replace(`motion-move-2`, `motion-move-1`);
       }
       if (item.classList.contains('box-6')) {
-        item.classList.replace(`motion-move-2`, `motion-move-3`);
+        item.classList.replace(`motion-move-3`, `motion-move-2`);
       }
     }
     if (limit === 5) {
       if (item.classList.contains('box-1')) {
-        item.classList.replace(`motion-move-4`, `motion-move-5`);
+        item.classList.replace(`motion-move-3`, `motion-move-2`);
       }
       if (item.classList.contains('box-2')) {
-        item.classList.replace(`motion-move-5`, `motion-move-6`);
+        item.classList.replace(`motion-move-4`, `motion-move-3`);
       }
       if (item.classList.contains('box-3')) {
-        item.classList.replace(`motion-move-6`, `motion-move-1`);
+        item.classList.replace(`motion-move-5`, `motion-move-4`);
       }
       if (item.classList.contains('box-4')) {
-        item.classList.replace(`motion-move-1`, `motion-move-2`);
+        item.classList.replace(`motion-move-6`, `motion-move-5`);
       }
       if (item.classList.contains('box-5')) {
-        item.classList.replace(`motion-move-2`, `motion-move-3`);
+        item.classList.replace(`motion-move-1`, `motion-move-6`);
       }
       if (item.classList.contains('box-6')) {
-        item.classList.replace(`motion-move-3`, `motion-move-4`);
+        item.classList.replace(`motion-move-2`, `motion-move-1`);
       }
     }
     if (limit === 6) {
       if (item.classList.contains('box-1')) {
-        item.classList.replace(`motion-move-5`, `motion-move-6`);
+        item.classList.replace(`motion-move-2`, `motion-move-1`);
       }
       if (item.classList.contains('box-2')) {
-        item.classList.replace(`motion-move-6`, `motion-move-1`);
+        item.classList.replace(`motion-move-3`, `motion-move-2`);
       }
       if (item.classList.contains('box-3')) {
-        item.classList.replace(`motion-move-1`, `motion-move-2`);
+        item.classList.replace(`motion-move-4`, `motion-move-3`);
       }
       if (item.classList.contains('box-4')) {
-        item.classList.replace(`motion-move-2`, `motion-move-3`);
+        item.classList.replace(`motion-move-5`, `motion-move-4`);
       }
       if (item.classList.contains('box-5')) {
-        item.classList.replace(`motion-move-3`, `motion-move-4`);
+        item.classList.replace(`motion-move-6`, `motion-move-5`);
       }
       if (item.classList.contains('box-6')) {
-        item.classList.replace(`motion-move-4`, `motion-move-5`);
-      }
-    }
-    if (limit === 7) {
-      if (item.classList.contains('box-1')) {
-        item.classList.replace(`motion-move-6`, `motion-move-1`);
-      }
-      if (item.classList.contains('box-2')) {
-        item.classList.replace(`motion-move-1`, `motion-move-2`);
-      }
-      if (item.classList.contains('box-3')) {
-        item.classList.replace(`motion-move-2`, `motion-move-3`);
-      }
-      if (item.classList.contains('box-4')) {
-        item.classList.replace(`motion-move-3`, `motion-move-4`);
-      }
-      if (item.classList.contains('box-5')) {
-        item.classList.replace(`motion-move-4`, `motion-move-5`);
-      }
-      if (item.classList.contains('box-6')) {
-        item.classList.replace(`motion-move-5`, `motion-move-6`);
+        item.classList.replace(`motion-move-1`, `motion-move-6`);
       }
     }
   });
