5e7fe13ffea5cbcee96c8391f17b1ba3c9744bab
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-02-27 13:56:27 +0200

    snap.svg

--- js/mobile-menu.js
+++
@@ -1,5 +0,0 @@
-(() => {
-  const mobileMenu = document.querySelector('.js-menu-container');
-  const openMenuBtn = document.querySelector('.js-open-menu');
-  const closeMenuBtn = document.querySelector('.js-close-menu');
-})();
