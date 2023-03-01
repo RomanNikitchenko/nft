6210ebb3a1240f392ba33590306b7d30bffd6b00
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-03-01 17:57:31 +0200

    screenWidthReset

--- js/animeAxisRotation.js
+++ js/animeAxisRotation.js
@@ -1,53 +1,51 @@
 (() => {
-    const btnMove = document.querySelector('.btnMove');
-    
-    let disabled = false;
-    let limit = 1;
+    var disabled = false;
+    var limit = 1;
 
     //anime.js
-    let path1 = anime.path('.svg #svg_1');
-    let path2 = anime.path('.svg #svg_2');
-    let path3 = anime.path('.svg #svg_3');
-    let path4 = anime.path('.svg #svg_4');
-    let path5 = anime.path('.svg #svg_5');
-    let path6 = anime.path('.svg #svg_6');
+    var path1 = anime.path('#svg_1');
+    var path2 = anime.path('#svg_2');
+    var path3 = anime.path('#svg_3'); 
+    var path4 = anime.path('#svg_4');
+    var path5 = anime.path('#svg_5'); 
+    var path6 = anime.path('#svg_6');
 
-    let motionMave1 = anime({
+    var motionMave1 = anime({
         targets: ".target-1",
         translateX: path1('x'),
         translateY: path1('y'),
         autoplay: false,
     });
 
-    let motionMave2 = anime({
+    var motionMave2 = anime({
         targets: ".target-2",
         translateX: path2('x'),
         translateY: path2('y'),
         autoplay: false,
     });
 
-    let motionMave3 = anime({
+    var motionMave3 = anime({
         targets: ".target-3",
         translateX: path3('x'),
         translateY: path3('y'),
         autoplay: false,
     });
 
-    let motionMave4 = anime({
+    var motionMave4 = anime({
         targets: ".target-4",
         translateX: path4('x'),
         translateY: path4('y'),
         autoplay: false,
     });
 
-    let motionMave5 = anime({
+    var motionMave5 = anime({
         targets: ".target-5",
         translateX: path5('x'),
         translateY: path5('y'),
         autoplay: false,
     });
 
-    let motionMave6 = anime({
+    var motionMave6 = anime({
         targets: ".target-6",
         translateX: path6('x'),
         translateY: path6('y'),
@@ -70,66 +68,66 @@
         motionMove.play();
     }
 
-    const byCondition = () => {
-        if (limit === 1) {
-            changePath(motionMave1, ".target-2", path1);
-            changePath(motionMave2, ".target-3", path2);
-            changePath(motionMave3, ".target-4", path3);
-            changePath(motionMave4, ".target-5", path4);
-            changePath(motionMave5, ".target-6", path5);
-            changePath(motionMave6, ".target-1", path6);
-        }
-        if (limit === 2) {
-            changePath(motionMave1, ".target-3", path1);
-            changePath(motionMave2, ".target-4", path2);
-            changePath(motionMave3, ".target-5", path3);
-            changePath(motionMave4, ".target-6", path4);
-            changePath(motionMave5, ".target-1", path5);
-            changePath(motionMave6, ".target-2", path6);
-        }
-        if (limit === 3) {
-            changePath(motionMave1, ".target-4", path1);
-            changePath(motionMave2, ".target-5", path2);
-            changePath(motionMave3, ".target-6", path3);
-            changePath(motionMave4, ".target-1", path4);
-            changePath(motionMave5, ".target-2", path5);
-            changePath(motionMave6, ".target-3", path6);
-        }
-        if (limit === 4) {
-            changePath(motionMave1, ".target-5", path1);
-            changePath(motionMave2, ".target-6", path2);
-            changePath(motionMave3, ".target-1", path3);
-            changePath(motionMave4, ".target-2", path4);
-            changePath(motionMave5, ".target-3", path5);
-            changePath(motionMave6, ".target-4", path6);
-        }
-        if (limit === 5) {
-            changePath(motionMave1, ".target-6", path1);
-            changePath(motionMave2, ".target-1", path2);
-            changePath(motionMave3, ".target-2", path3);
-            changePath(motionMave4, ".target-3", path4);
-            changePath(motionMave5, ".target-4", path5);
-            changePath(motionMave6, ".target-5", path6);
-        }
-        if (limit === 6) {
-            changePath(motionMave1, ".target-1", path1);
-            changePath(motionMave2, ".target-2", path2);
-            changePath(motionMave3, ".target-3", path3);
-            changePath(motionMave4, ".target-4", path4);
-            changePath(motionMave5, ".target-5", path5);
-            changePath(motionMave6, ".target-6", path6);
+    const screenWidthchangePath = (motionMove, target, path) => {
+        motionMove = anime({
+            targets: target,
+            translateX: path('x'),
+            translateY: path('y'),
+            direction: 'reverse',
+            autoplay: false,
+        });
+    }
+
+    const byCondition = (callback) => {
+        const targetClasses = [".target-2", ".target-3", ".target-4", ".target-5", ".target-6", ".target-1"];
+        for (var i = 0; i < targetClasses.length; i++) {
+            if (limit === i + 1) {
+                callback(motionMave1, targetClasses[i], path1);
+                callback(motionMave2, targetClasses[(i + 1) % 6], path2);
+                callback(motionMave3, targetClasses[(i + 2) % 6], path3);
+                callback(motionMave4, targetClasses[(i + 3) % 6], path4);
+                callback(motionMave5, targetClasses[(i + 4) % 6], path5);
+                callback(motionMave6, targetClasses[(i + 5) % 6], path6);
+                break;
+            }
         }
     }
-    btnMove.addEventListener('click', e => {
+
+    const toggleElementsAnimation = (e) => {
+        if (e.target.classList.contains("logo")) return;
+        if (e.target.classList.contains("logo__accent")) return;
+        if (e.target.tagName === 'A') return;
+        if (e.currentTarget.tagName !== 'BODY') return;
+        
         e.preventDefault();
 
         if (disabled) return;
 
-        if (e.target.tagName !== 'BUTTON') return;
+        console.log(e.currentTarget.tagName);
+        console.log(e.target.tagName);
 
         disabled = true;
 
-        byCondition();
+        byCondition(changePath);
+
+        if (limit === 6) {
+            limit = 1;
+        } else {
+            limit += 1;
+        }
+    };
+
+    document.body.addEventListener('click', toggleElementsAnimation);
+    
+    window.addEventListener('touchstart', function (e) {
+        if (e.target.classList.contains("logo")) return;
+        if (e.target.classList.contains("logo__accent")) return;
+        if (e.target.tagName === 'A') return;
+        if (disabled) return;
+
+        disabled = true;
+
+        byCondition(changePath);
 
         if (limit === 6) {
             limit = 1;
@@ -137,4 +135,19 @@
             limit += 1;
         }
     });
+
+    const screenWidthReset = () => {
+        path1 = anime.path('#svg_1');
+        path2 = anime.path('#svg_2');
+        path3 = anime.path('#svg_3'); 
+        path4 = anime.path('#svg_4');
+        path5 = anime.path('#svg_5'); 
+        path6 = anime.path('#svg_6');
+
+        byCondition(screenWidthchangePath);
+    }
+
+
+    // Добавление обработчика события resize для перезаписи path от 1 до 6 при изменении размера экрана
+    window.addEventListener('resize', screenWidthReset);
 })();
--- js/svgSegments.js
+++ js/svgSegments.js
@@ -1,43 +1,83 @@
 (() => {
-    // Создание объекта Snap из SVG-элемента
-    var s = Snap('.svg');
+    // Функция для перезаписи SVG на новый
+    function redrawSVG() {
+        var screenWidth = window.innerWidth;
+        var svgWidth, svgHeight, pathD;
 
-    // Получение элемента path
-    var path = s.select("#svg_path");
+        // Определение ширины, высоты и path для каждого экрана
+        if (screenWidth <= 767) { // Для экранов шириной до 767 пикселей (мобильные)
+            svgWidth = "320";
+            svgHeight = "168";
+            pathD = "M1195.56 245.882C1205.12 330.051 1147.16 413.944 1045.43 481.524C943.73 549.088 798.407 600.252 633.567 618.972C468.728 637.692 315.632 620.418 201.37 577.38C87.0809 534.331 11.7864 465.571 2.22789 381.403C-7.33066 297.234 50.6277 213.341 152.354 145.761C254.055 78.1973 399.378 27.0334 564.218 8.31341C729.057 -10.4066 882.153 6.86738 996.415 49.9053C1110.7 92.9537 1186 161.714 1195.56 245.882Z";
+        } else if (screenWidth >= 768 && screenWidth <= 1529) { // Для экранов шириной до 1529 пикселей (планшеты)
+            svgWidth = "668";
+            svgHeight = "350";
+            pathD = "M1195.56 245.882C1205.12 330.051 1147.16 413.944 1045.43 481.524C943.73 549.088 798.407 600.252 633.567 618.972C468.728 637.692 315.632 620.418 201.37 577.38C87.0809 534.331 11.7864 465.571 2.22789 381.403C-7.33066 297.234 50.6277 213.341 152.354 145.761C254.055 78.1973 399.378 27.0334 564.218 8.31341C729.057 -10.4066 882.153 6.86738 996.415 49.9053C1110.7 92.9537 1186 161.714 1195.56 245.882Z";
+        } else if (screenWidth >= 1530) { // Для экранов шириной больше 1530 пикселей (десктопы)
+            svgWidth = "1198";
+            svgHeight = "627";
+            pathD = "M1195.56 245.882C1205.12 330.051 1147.16 413.944 1045.43 481.524C943.73 549.088 798.407 600.252 633.567 618.972C468.728 637.692 315.632 620.418 201.37 577.38C87.0809 534.331 11.7864 465.571 2.22789 381.403C-7.33066 297.234 50.6277 213.341 152.354 145.761C254.055 78.1973 399.378 27.0334 564.218 8.31341C729.057 -10.4066 882.153 6.86738 996.415 49.9053C1110.7 92.9537 1186 161.714 1195.56 245.882Z";
+        }
 
-    // Получение общей длины path
-    var pathLength = Snap.path.getTotalLength(path);
-
-    // Разбиение path на 6 сегментов
-    var segmentLengths = [
-        pathLength * 0.25,
-        pathLength * 0.125,
-        pathLength * 0.125,
-        pathLength * 0.25,
-        pathLength * 0.125,
-        pathLength * 0.125,
-    ];
-
-    // Получение начала и конца каждого сегмента
-    var segmentStartsAndEnds = [
-        [0, segmentLengths[0]],
-        [segmentLengths[0], segmentLengths[0] + segmentLengths[1]],
-        [segmentLengths[0] + segmentLengths[1], segmentLengths[0] + segmentLengths[1] + segmentLengths[2]],
-        [segmentLengths[0] + segmentLengths[1] + segmentLengths[2], segmentLengths[0] + segmentLengths[1] + segmentLengths[2] + segmentLengths[3]],
-        [segmentLengths[0] + segmentLengths[1] + segmentLengths[2] + segmentLengths[3], segmentLengths[0] + segmentLengths[1] + segmentLengths[2] + segmentLengths[3] + segmentLengths[4]],
-        [segmentLengths[0] + segmentLengths[1] + segmentLengths[2] + segmentLengths[3] + segmentLengths[4], pathLength]
-    ];
-
-    // Создание новых path для каждого сегмента
-    for (var i = 0; i < segmentStartsAndEnds.length; i++) {
-        var segment = Snap.path.getSubpath(path, segmentStartsAndEnds[i][0], segmentStartsAndEnds[i][1]);
-        var segmentPath = s.path(segment);
-        // Установка атрибутов каждого сегмента
-        segmentPath.attr({
-            id: `svg_${i + 1}`,
-            fill: "none",
-            stroke: "#000",
-            strokeWidth: 1
+        // Создание нового SVG
+        var s = Snap("#svg-container");
+        s.attr({
+            width: svgWidth,
+            height: svgHeight
         });
+
+        // Создание нового path
+        var path = s.select("#svg_path");
+        path.attr({
+            d: pathD
+        });
+
+        // Получение общей длины path
+        var pathLength = Snap.path.getTotalLength(path);
+
+        // Разбиение path на 6 сегментов
+        var segmentLengths = [
+            pathLength * 0.25,
+            pathLength * 0.125,
+            pathLength * 0.125,
+            pathLength * 0.25,
+            pathLength * 0.125,
+            pathLength * 0.125,
+        ];
+
+        // Получение начала и конца каждого сегмента
+        var segmentStartsAndEnds = [];
+        var segmentStart = 0;
+        for (var i = 0; i < segmentLengths.length; i++) {
+            var segmentEnd = segmentStart + segmentLengths[i];
+            segmentStartsAndEnds.push([segmentStart, segmentEnd]);
+            segmentStart = segmentEnd;
+        }
+
+        // Создание новых path для каждого сегмента
+        for (var i = 0; i < segmentStartsAndEnds.length; i++) {
+            // Получаем элемент path текущего сегмента, если он уже существует
+            var segmentPath = s.select(`#svg_${i + 1}`);
+            // Если элемент path уже существует, то удаляем его
+            if (segmentPath) {
+                segmentPath.remove();
+            }
+            // Создаем новый элемент path для текущего сегмента
+            var segment = Snap.path.getSubpath(path, segmentStartsAndEnds[i][0], segmentStartsAndEnds[i][1]);
+            segmentPath = s.path(segment);
+            // Установка атрибутов каждого сегмента
+            segmentPath.attr({
+                id: `svg_${i + 1}`,
+                fill: "none",
+                stroke: "#000",
+                strokeWidth: 1
+            });
+        }
     }
+
+    // Вызов функции для перезаписи SVG при загрузке страницы
+    redrawSVG();
+
+    // Добавление обработчика события resize для перезаписи SVG при изменении размера экрана
+    window.addEventListener('resize', redrawSVG);
 })();
