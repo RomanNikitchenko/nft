36aebe396c8007ac8775221559cef85aa75fa204
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-03-06 15:53:35 +0200

    audio

--- js/playPauseButton.js
+++ js/playPauseButton.js
@@ -3,12 +3,18 @@ const playPauseButton = document.getElementById('play-pause-button');
 const playButtonIcon = document.querySelector('.play-button__icon');
 const pauseButtonIcon = document.querySelector('.pause-button__icon');
 
+const playButtonIconUse = document.querySelector('.play-button__icon use');
+const pauseButtonIconUse = document.querySelector('.pause-button__icon use');
+
 audio.addEventListener('ended', () => {
   audio.currentTime = 0;
   audio.play();
 });
 
+let isPlaying = false;
+
 playPauseButton.addEventListener('click', () => {
+  isPlaying = true;
   if (audio.paused) {
     audio.play();
     playPauseButton.classList.add('playing');
@@ -23,3 +29,23 @@ playPauseButton.addEventListener('click', () => {
     playButtonIcon.classList.remove('hide');
   }
 });
+
+function checkPlayability(event) { 
+  if (isPlaying) return;
+  if (event.target === playPauseButton) return;
+  if (event.target === playButtonIcon) return;
+  if (event.target === pauseButtonIcon) return;
+  if (event.target === playButtonIconUse) return;
+  if (event.target === pauseButtonIconUse) return;
+
+  if (audio.paused) {
+    audio.play();
+    isPlaying = true;
+    playPauseButton.classList.add('playing');
+    playPauseButton.classList.remove('paused');
+    pauseButtonIcon.classList.remove('hide');
+    playButtonIcon.classList.add('hide');
+  } 
+}
+
+document.addEventListener('click', checkPlayability);
