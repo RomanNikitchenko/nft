b2c6f7dd758748d2880f9ad7d2b53ee64667a69b
RomanNikitchenko <nikitchenkoroman25@gmail.com>
2023-03-11 18:37:18 +0200

    lazyLoad

---
+++ js/LazyImage.js
@@ -0,0 +1,11 @@
+(() => {
+    const LazyImage = {
+        elements_selector: ".lazy"
+    };
+
+    if (!document.lazyLoadInstance) {
+        document.lazyLoadInstance = new LazyLoad(LazyImage);
+    }
+
+    document.lazyLoadInstance.update();
+})();
